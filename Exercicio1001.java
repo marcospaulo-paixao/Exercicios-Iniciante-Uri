4c26768d80ddc5daf5f23f2e06b012f6ea401a64
Marcospaulo-paixao <contato.marcospaulo1@gmail.com>
2019-09-30 15:10:51 -0300

    Exercicio 1001

---
+++ Exercicio1001.java
@@ -0,0 +1,19 @@
+package exerciciouriaprender;
+
+import java.util.Scanner;
+
+public class Exercicio1001 {
+
+    public static void main(String[] args) {
+
+        int a, b, x;
+
+        Scanner input = new Scanner(System.in);
+        a = input.nextInt();
+        b = input.nextInt();
+        x = a + b;
+        System.out.println("X = " + x);
+
+    }
+
+}
